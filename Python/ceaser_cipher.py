c8d2e7baca591f26d9e9a0572080bb5e355f7264
JIP5474 <jpjishnuprasanth@gmail.com>
2020-11-26 08:11:08 +0530

    Create ceaser_cipher.py

---
+++ Python/ceaser_cipher.py
@@ -0,0 +1,29 @@
+#program to perform ceaser cipher
+
+ch='y'
+while ch=='y' or ch=='Y':
+    string=input("Enter the string: ")
+    print("Menu:")                      #menu for asking choice
+    print("1.Cipher a message")
+    print("2.Decipher a message")
+    choice=int(input("Enter your choice: "))
+    if choice==1:
+        k=int(input("Enter the key for ciphering: "))
+        print("Creating cipher")
+        code=""
+        for x in string:
+            code=code+chr(ord(x)+k)
+        print("The code is ",code)
+    elif choice==2:
+        k=int(input("Enter the key for deciphering: "))
+        print("Deciphering code")
+        message=""
+        for x in string:
+            message=message+chr(ord(x)-k)
+        print("The message is ",message)
+    else:
+        print("Wrong choice")
+    ch=input("Do you wish to continue?(y/n):")      #asking to continue or not
+
+            
+            
